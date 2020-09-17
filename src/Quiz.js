90178aa41541d4b83194cb9c2fe2d0ea9c76f1bf
annalunder <31619638+annalunder@users.noreply.github.com>
2020-09-17 12:50:12 +0200

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion.js'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
-let quizData = require('./quiz_data.json')
-
-class Quiz extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { quiz_position: 1 }
-  }
+class QuizQuestion extends Component {
   render() {
     return (
-      <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-      </div>
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+          </ul>
+        </section>
+      </main>      
     )
   }
 }
 
-export default Quiz
+export default QuizQuestion
